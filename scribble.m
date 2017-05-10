b8ecf104ab9459bf0892d078516492b613b6c35f
Kang-Sun-CfA <franksun1016@gmail.com>
2017-05-10 15:34:17 -0400

    plot L3 compare
    changes made in my laptop

--- scribble.m
+++ scribble.m
@@ -1,4 +1,5 @@
-validmask = true(60,1644);
+validmask = false(60,1644);
+validmask(60,300) = true;
     Lat_lowerleft = datavar.PixelCornerLatitudes.data(1:end-1,1:end-1);
     Lat_lowerright = datavar.PixelCornerLatitudes.data(2:end,1:end-1);
     Lat_upperleft = datavar.PixelCornerLatitudes.data(1:end-1,2:end);
@@ -31,4 +32,44 @@ validmask = true(60,1644);
         tempLon_lowerleft(:),tempLon_upperleft(:),...
         tempLon_upperright(:),tempLon_lowerright(:),tempLonC(:),...
         ];
- 
+%% 
+
+Lon_r = tempdata(6:9);
+Lat_r = tempdata(1:4);
+Lon_c = tempdata(10);
+Lat_c = tempdata(5);
+Res = 0.05;
+pixel.nv = length(Lon_r);
+pixel.vList = [(Lon_r(:)-Lon_left)/Res,(Lat_r(:)-Lat_low)/Res];
+
+Lon_left = floor(min(Lon_r));
+Lon_right = ceil(max(Lon_r));
+Lat_low = floor(min(Lat_r));
+Lat_up = ceil(max(Lat_r));
+
+
+
+[pout,npout] = F_HcakeCut(pixel);
+close all;
+subplot(1,2,1)
+hold on
+for i = 1:npout
+    F_plot_polygon(pout(i));
+end
+[pout,npout] = F_VcakeCut(pixel);
+axis equal
+subplot(1,2,2)
+hold on
+for i = 1:npout
+    F_plot_polygon(pout(i));
+end
+axis equal
+% test clockwise
+figure
+hold on
+plot(Lon_c,Lat_c,'ok')
+for i = 1:length(Lon_r)
+    plot(Lon_r(i),Lat_r(i),'*');
+end
+hold off
+axis equal
--- test_cakecut.m
+++ test_cakecut.m
@@ -13,7 +13,10 @@ outputdir = '/data/tempo1/Shared/kangsun/Oversampling/RegridPixels/output/';
 rundir = '/data/tempo1/Shared/kangsun/Oversampling/RegridPixels/';
 
 % directory for plot
-plotdir = '/data/tempo1/Shared/kangsun/Oversampling/RegridPixels/plot/';
+plotdir = '/home/kangsun/Oversampling_matlab/plot/';
+
+% matlab code dir
+codedir = '/home/kangsun/Oversampling_matlab';
 
 addpath('/home/kangsun/matlab functions/export_fig/')
 addpath('/home/kangsun/matlab functions/')
@@ -33,9 +36,9 @@ Startdate = [2005 7 1];
 Enddate = [2005 7 31];
 % step in dates, not very useful in this project
 % Step = 1;
-
-% do you wanna use destriped OMI data?
-if_destripe = true;
+% 
+% % do you wanna use destriped OMI data?
+% if_destripe = true;
 
 % lat lon box
 MinLat = 38; MaxLat = 43;
@@ -191,7 +194,7 @@ fid = fopen(input_fn,'w');
 fprintf(fid,['%8d',repmat('%15.6f',1,13),repmat('%15.6E',1,2),'\n'],savedata');
 fclose(fid);
 %%
-cd /data/tempo1/Shared/kangsun/Oversampling/RegridPixels/matlab_oversampling/
+cd codedir
 np = size(savedata,1);
 LAT = savedata(:,2:6);LON = savedata(:,7:11);
 Lon_left = floor(min(LON(:)));
@@ -210,173 +213,65 @@ Sum_Below = zeros(nrows,ncols);
 
 options = [];
 
-options.use_SRF = true;
+options.use_SRF = false;
 options.m = 4;
 options.n = 2;
-options.use_simplified_area = true;
-options.inflate_pixel = true;
+options.use_simplified_area = false;
+options.inflate_pixel = false;
 options.inflationx = 1.5;
 options.inflationy = 2;
+
+disp('Base case')
 tic
-parfor ip = 1:np
-    Lon_r = LON(ip,1:4);
-    Lat_r = LAT(ip,1:4);
-    Lon_c = LON(ip,5);
-    Lat_c = LAT(ip,5);
-    
-if ~isfield(options,'inflate_pixel')
-    inflate_pixel = false;
-else
-    inflate_pixel = options.inflate_pixel;
-end
-
-if ~isfield(options,'use_SRF')
-    use_SRF = false;
-else
-    use_SRF = options.use_SRF;
-end
-
-if ~isfield(options,'use_simplified_area')
-    use_simplified_area = false;
-else
-    use_simplified_area = options.use_simplified_area;
-end
-pixel = [];
-pixel.nv = length(Lon_r);
-pixel.vList = [(Lon_r(:)-Lon_left)/Res,(Lat_r(:)-Lat_low)/Res];
-pixel.center = [(Lon_c-Lon_left)/Res,(Lat_c-Lat_low)/Res];
-vList = pixel.vList;
-
-% inflate_pixel and use_SRF should be used together, but don't have to
-if ~use_SRF % if not use SRF, there is no point of inflating!
-    inflate_pixel = false;
-end
-
-if inflate_pixel || use_SRF
-    leftpoint = mean(vList(1:2,:));
-    rightpoint = mean(vList(3:4,:));
-    
-    uppoint = mean(vList(2:3,:));
-    lowpoint = mean(vList([1 4],:));
-    
-    % calculate the FWHM of 2-D super gaussian SRF
-    % x is the xtrack, different from the OMI pixel paper, which used y
-    FWHMx = sqrt((leftpoint(1)-rightpoint(1))^2+(leftpoint(2)-rightpoint(2))^2);
-    % y is the along track, different from the OMI pixel paper, which used x
-    FWHMy = sqrt((uppoint(1)-lowpoint(1))^2+(uppoint(2)-lowpoint(2))^2);
-    
-    Angle = -atan((rightpoint(2)-leftpoint(2))/(rightpoint(1)-leftpoint(1)));
-    rotation_matrix = [cos(Angle), -sin(Angle);
-        sin(Angle),  cos(Angle)];
-end
-
-% m is the super gaussian exponient in x (xtrack), n is in y (along track)
-if use_SRF
-    if ~isfield(options,'m')
-        m = 4;
-    else
-        m = options.m;
-    end
-    if ~isfield(options,'n')
-        n = 2;
-    else
-        n = options.n;
-    end
-end
-
-if inflate_pixel % if inflate the pixel, update the pixel object
-    % inflation factor xtrack
-    if ~isfield(options,'inflationx')
-        inflationx = 1.5;
-    else
-        inflationx = options.inflationx;
-    end
-    % inflation factor along track
-    if ~isfield(options,'inflationy')
-        inflationy = 2;
-    else
-        inflationy = options.inflationy;
-    end
-    
-    diamond_orth = (rotation_matrix*...
-        [[leftpoint(1) uppoint(1) rightpoint(1) lowpoint(1)]-pixel.center(1);...
-        [leftpoint(2) uppoint(2) rightpoint(2) lowpoint(2)]-pixel.center(2)]);
-    
-    xleft = diamond_orth(1,1)*inflationx;
-    xright = diamond_orth(1,3)*inflationx;
-    ytop = diamond_orth(2,2)*inflationy;
-    ybottom = diamond_orth(2,4)*inflationy;
-    
-    vList_orth = [xleft xleft xright xright;
-        ybottom ytop ytop ybottom];
-    
-    vList_inflate = [cos(-Angle), -sin(-Angle);
-        sin(-Angle),  cos(-Angle)]*vList_orth;
-    
-    vList_inflate = [vList_inflate(1,:)'+pixel.center(1),vList_inflate(2,:)'+pixel.center(2)];
-    pixel.vList = vList_inflate;
-end
-% Used for the next step
-id_all = 0;
-pixel_area = 0;
-
-% Perform Horizontal cut first at the integer grid lines
-[sub_pixels,n_sub_pixels] = F_HcakeCut( pixel );
-
-% Sub_Area = zeros(size(Sum_Above));
-Sub_Area = zeros(nrows,ncols);
-% Then perform Vertical cut for each sub pixel obtainted
-% from the Horizontal cut at the integer grid lines
-for id_sub = 1: n_sub_pixels
-    [final_pixels, n_final_pixels] = F_VcakeCut( sub_pixels(id_sub) );
-    for id_final = 1: n_final_pixels
-        id_all = id_all + 1;
-        % temp_area is the area of each sub polygon, at this stage
-        if use_simplified_area
-            temp_area = Res^2;
-        else
-            [ifsquare,edges] = F_if_square(final_pixels(id_final));
-            if ifsquare
-                temp_area = edges(1)*edges(2)*Res^2;
-            else
-                temp_area = F_polyarea(final_pixels(id_final))*Res^2;
-            end
-        end
-        row = floor(min(final_pixels(id_final).vList(1:final_pixels(id_final).nv,2))) + 1;
-        col = floor(min(final_pixels(id_final).vList(1:final_pixels(id_final).nv,1))) + 1;
-        
-        % if necessary, update temp_area to include the weighting from SRF
-        if use_SRF
-            temp_area = temp_area * ...
-                F_2D_SG(col,row,pixel.center(1),pixel.center(2),FWHMx,FWHMy,m,n,rotation_matrix);
-        end
-        pixel_area = pixel_area + temp_area;
-        
-        % Get the overlaped area between the pixel and each cell
-        Sub_Area(row,col) = temp_area;
-        
-%         Pixels_count(row,col) = Pixels_count(row,col) + 1;
-    end
-end
-% Sum weighted value and weights
-% Here use temp_area/A/VCD_Unc(p) as averaging weight, meaning that
-% averaging weight is assumed to be proportional to the ratio of the overlap area (temp_area) to the
-% pixel size (A) and inversely proportional to the error standard deviation (VCD_Unc(p)).
-% If you just want fraction of overlap area as averaging weight, use: temp_area/A
-% If you just want area weighted average, use: temp_area
-Sum_Above = Sum_Above+Sub_Area/pixel_area/VCD_Unc(ip)*VCD(ip);
-Sum_Below = Sum_Below+Sub_Area/pixel_area/VCD_Unc(ip);
-
-end
+S_cakeCut
 toc
-Sum_Below(Sum_Below == 0) = nan;
-Average = Sum_Above./Sum_Below;
-%%
-clc
-count = 0;
-parfor i = 1:12
-    count = sum(count);
-end
+plotcell{1} = Average;
+
+options.use_simplified_area = true;
+disp('case 2')
+tic
+S_cakeCut
+toc
+plotcell{2} = Average;
+
+options.use_SRF = true;
+options.use_simplified_area = false;
+options.inflate_pixel = true;
+disp('case 3')
+tic
+S_cakeCut
+toc
+plotcell{3} = Average;
+
+options.use_SRF = true;
+options.use_simplified_area = true;
+options.inflate_pixel = true;
+disp('case 4')
+tic
+S_cakeCut
+toc
+plotcell{4} = Average;
+
+options.use_SRF = true;
+options.use_simplified_area = false;
+options.inflate_pixel = true;
+options.m = 2;
+options.n = 2;
+disp('case 5')
+tic
+S_cakeCut
+toc
+plotcell{5} = Average;
+
+options.use_SRF = true;
+options.use_simplified_area = true;
+options.inflate_pixel = true;
+disp('case 6')
+tic
+S_cakeCut
+toc
+plotcell{6} = Average;
+
 %%
 Lat_grid = Lat_low+(1:nrows)*Res-0.5*Res;
 Lon_grid = Lon_left+(1:ncols)*Res-0.5*Res;
@@ -398,13 +293,16 @@ for i = 1:length(Llake)
 end
 end
 % close all
-figure('color','w','unit','inch','position',[0 1 10 8])
-set(0,'defaultaxesfontsize',13)
+figure('color','w','unit','inch','position',[0 1 10 6])
+set(0,'defaultaxesfontsize',12)
 % h = scatter(C(:,4),C(:,3),[],C(:,5));
-plotmat = Average;
+for i = 1:6
+    subplot(3,2,i)
+plotmat = plotcell{i};
 
 h = pcolor(Lon_grid,Lat_grid,plotmat);set(h,'edgecolor','none')
 hc = colorbar;
+axis equal
 caxis([0 2e16])
 hold on
 for istate = 1:length(S)
@@ -414,40 +312,42 @@ end
 for ilake = lakelist
     plot(Llake(ilake).X,Llake(ilake).Y,'color','w')
 end
-%%
-disp('Running Lei Zhu''s pixel-regriding program ...')
-fprintf('\n')
-tic
-output_fn = ['Res_',num2str(Res),'_Lat_',num2str(MinLat),'_',...
-    num2str(MaxLat),'_Lon_',...
-    num2str(MinLon),'_',num2str(MaxLon),'_Year_',num2str(Startdate(1,1)),...
-    num2str(Startdate(1,2),'%02d'),'_',...
-    num2str(Enddate(end,1)),num2str(Enddate(end,2),'%02d'),'.dat'];
-cd(rundir)
-
-% I know this is stupid, give me a better way
-
-fid = fopen('run_KS.sh','w');
-fprintf(fid,['set Input_Dir = "',inputdir(1:end),'"\n']);
-fprintf(fid,['set Output_Dir = "',outputdir(1:end),'"\n']);
-
-fprintf(fid,['set Res = ',num2str(Res),'\n']);
-
-fprintf(fid,['set Input_Filename = "',input_fn,'"\n']);
-fprintf(fid,['set Output_Filename = "',output_fn,'"\n']);
-
-fprintf(fid,['./RegridPixels.x<<EOF\n',...
-    '$Input_Dir\n',...
-    '$Output_Dir \n',...
-    '$Input_Filename\n',...
-    '$Output_Filename\n',...
-    '$Res\n',...
-    'EOF\n',...
-    'quit:\n',...
-    'exit']);
-fclose(fid);
-
-unix('tcsh ./run_KS.sh');
-tt = toc;
-disp(['Took ',num2str(tt),' s'])
-fprintf('\n')
+end
+export_fig([plotdir,'L3_compare.pdf'])
+% %%
+% disp('Running Lei Zhu''s pixel-regriding program ...')
+% fprintf('\n')
+% tic
+% output_fn = ['Res_',num2str(Res),'_Lat_',num2str(MinLat),'_',...
+%     num2str(MaxLat),'_Lon_',...
+%     num2str(MinLon),'_',num2str(MaxLon),'_Year_',num2str(Startdate(1,1)),...
+%     num2str(Startdate(1,2),'%02d'),'_',...
+%     num2str(Enddate(end,1)),num2str(Enddate(end,2),'%02d'),'.dat'];
+% cd(rundir)
+% 
+% % I know this is stupid, give me a better way
+% 
+% fid = fopen('run_KS.sh','w');
+% fprintf(fid,['set Input_Dir = "',inputdir(1:end),'"\n']);
+% fprintf(fid,['set Output_Dir = "',outputdir(1:end),'"\n']);
+% 
+% fprintf(fid,['set Res = ',num2str(Res),'\n']);
+% 
+% fprintf(fid,['set Input_Filename = "',input_fn,'"\n']);
+% fprintf(fid,['set Output_Filename = "',output_fn,'"\n']);
+% 
+% fprintf(fid,['./RegridPixels.x<<EOF\n',...
+%     '$Input_Dir\n',...
+%     '$Output_Dir \n',...
+%     '$Input_Filename\n',...
+%     '$Output_Filename\n',...
+%     '$Res\n',...
+%     'EOF\n',...
+%     'quit:\n',...
+%     'exit']);
+% fclose(fid);
+% 
+% unix('tcsh ./run_KS.sh');
+% tt = toc;
+% disp(['Took ',num2str(tt),' s'])
+% fprintf('\n')
