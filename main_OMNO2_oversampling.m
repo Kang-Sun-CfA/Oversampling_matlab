34aa7068574e964f81a93789a14f07537cc1d288
Kang-Sun-CfA <franksun1016@gmail.com>
2017-07-13 17:53:05 -0400

    test omno2 subset

--- main_OMNO2_oversampling.m
+++ main_OMNO2_oversampling.m
@@ -12,7 +12,10 @@ if ~exist(L3dir,'dir')
 end
 % %% Kang Sun's PC
 % L2dir = 'd:\Research_CfA\OMNO2\L2\';
+if_download = false;
+if_subset = true;
 %% download OMNO2
+if if_download
 inp_download = [];
 % CONUS
 inp_download.MinLat = 25;
@@ -35,31 +38,32 @@ for iyear = 2004:2017
     output_download = F_download_OMI(inp_download);
     save([L2dir,num2str(iyear),'output_download.mat'],'output_download')
 end
+end
+%%
+if if_subset
+inp_subset = [];
+% CONUS
+inp_subset.MinLat = 25;
+inp_subset.MaxLat = 50;
+inp_subset.MinLon = -130;
+inp_subset.MaxLon = -63;
+
+inp_subset.MaxCF = 0.3;
+inp_subset.MaxSZA = 75;
+
+inp_subset.usextrack = 1:60;
+
+inp_subset.L2dir = L2dir;
+
+for iyear = 2004:2004
+    inp_subset.Startdate = [iyear 1 1];
+    inp_subset.Enddate = [iyear 12 31];
+    output_subset = F_subset_OMNO2(inp_subset);
+
+    L2g_fn = ['CONUS_',num2str(iyear),'.mat'];
+    save([L2gdir,L2g_fn],'inp_subset','output_subset')
+end
+    
+end
 % %%
-% inp_subset = [];
-% % CONUS
-% inp_subset.MinLat = 25;
-% inp_subset.MaxLat = 50;
-% inp_subset.MinLon = -130;
-% inp_subset.MaxLon = -63;
-% 
-% inp_subset.MaxCF = 0.3;
-% inp_subset.MaxSZA = 75;
-% 
-% inp_subset.if_download_xml = true;
-% inp_subset.if_download_he5 = true;
-% inp_subset.if_delete_he5 = false;
-% 
-% inp_subset.swath_BDR_fn = '/data/tempo1/Shared/kangsun/OMNO2/Important_constant/OMI_BDR.mat';
-% inp_subset.url0 = 'https://aura.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level2/OMNO2.003/';
-% inp_subset.L2dir = L2dir;
-% 
-% for iyear = 2008:2008
-%     inp_subset.Startdate = [iyear 1 1];
-%     inp_subset.Enddate = [iyear 12 31];
-%     output_subset = F_download_subset_OMNO2(inp_subset);
-% 
-%     L2g_fn = ['CONUS_',num2str(iyear),'.mat'];
-%     save([L2gdir,L2g_fn],'inp_subset','output_subset')
-% end
 % 
