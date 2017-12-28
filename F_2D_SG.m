d53dcafa1bd84c90afe70c80f9d50581c8aa1eec
Kang-Sun-CfA <kang.sun@cfa.harvard.edu>
2017-12-28 13:05:31 -0500

    update SG, correct absolute value

--- F_2D_SG.m
+++ F_2D_SG.m
@@ -3,25 +3,24 @@ function SG = F_2D_SG(xmesh,ymesh,x0,y0,FWHMx,FWHMy,m,n,Angle)
 % the OMI pixel shape paper
 
 % written by Kang Sun on 2017/05/06
-
-% angle in rad
+% corrected on 2017/12/09 to take absolute values
 
 wx = FWHMx/2/(log(2))^(1/m);
 wy = FWHMy/2/(log(2))^(1/n);
 if ~exist('Angle','var')
-SG = exp(-((xmesh-x0)/wx).^m-((ymesh-y0)/wy).^n);
-return;
+    SG = exp(-(abs((xmesh-x0)/wx)).^m-(abs((ymesh-y0)/wy)).^n);
+    return;
 end
 if length(Angle) > 1
     xym1 = [xmesh(:)'-x0; ymesh(:)'-y0];
-xym2 = Angle*xym1;
-SG = exp(-((xym2(1,:))/wx).^m-((xym2(2,:))/wy).^n);
-SG = reshape(SG(:),size(xmesh,1),size(xmesh,2));
+    xym2 = Angle*xym1;
+    SG = exp(-(abs((xym2(1,:))/wx)).^m-abs((xym2(2,:)/wy)).^n);
+    SG = reshape(SG(:),size(xmesh,1),size(xmesh,2));
 else
-   rotation_matrix = [cos(Angle), -sin(Angle);
-    sin(Angle),  cos(Angle)]; 
-xym1 = [xmesh(:)'-x0; ymesh(:)'-y0];
-xym2 = rotation_matrix*xym1;
-SG = exp(-((xym2(1,:))/wx).^m-((xym2(2,:))/wy).^n);
-SG = reshape(SG(:),size(xmesh,1),size(xmesh,2));
+    rotation_matrix = [cos(Angle), -sin(Angle);
+        sin(Angle),  cos(Angle)];
+    xym1 = [xmesh(:)'-x0; ymesh(:)'-y0];
+    xym2 = rotation_matrix*xym1;
+    SG = exp(-(abs((xym2(1,:))/wx)).^m-abs((xym2(2,:)/wy)).^n);
+    SG = reshape(SG(:),size(xmesh,1),size(xmesh,2));
 end
