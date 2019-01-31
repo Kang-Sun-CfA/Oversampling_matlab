59d639a1e29c7230f430daf4e3b6866fe5724fba
Kang-Sun-CfA <kang.sun@cfa.harvard.edu>
2019-01-31 10:00:12 -0500

    popy, add patch xmesh

--- popy.py
+++ popy.py
@@ -8,6 +8,8 @@ Created on Sat Jan 26 15:50:30 2019
 import numpy as np
 # conda install -c conda-forge opencv 
 import cv2
+# conda install -c scitools/label/archive shapely
+from shapely.geometry import Polygon
 
 class popy(object):
     
@@ -20,32 +22,37 @@ class popy(object):
             k2 = 2
             k3 = 1
             error_model = "linear"
+            oversampling_list = ['vcd','albedo','cloud_fraction','cloud_height']
         elif(sensor_name == "IASI"):
             k1 = 2
             k2 = 2
             k3 = 9
             error_model = "square"
+            oversampling_list = {'vcd'}
         elif(sensor_name == "CrIS"):
             k1 = 2
             k2 = 2
             k3 = 4
             error_model = "log"
+            oversampling_list = ['vcd']
         elif(sensor_name == "OMPS"):
             k1 = 6
             k2 = 2
             k3 = 3
             error_model = "linear"
+            oversampling_list = ['vcd','albedo','cloud_fraction','cloud_height']
         else:
             k1 = 2
             k2 = 2
             k3 = 1
             error_model = "linear"
+            oversampling_list = ['vcd']
         
         self.k1 = k1
         self.k2 = k2
         self.k3 = k3
         self.error_model = error_model
-        
+        self.oversampling_list = oversampling_list
         self.grid_size = grid_size
         
         if east < west:
@@ -117,12 +124,39 @@ class popy(object):
         return sg
     
     def F_regrid(self,l2g_data):
+        
+        def F_reference2west(west,data):
+            if data.size > 1:
+                data = data-west
+                data[data < 0.] = data[data < 0.]+360.
+            else:
+                if data < 0:
+                    data = data+360
+            return data
+        
+        def F_lon_distance(lon1,lon2):
+            if lon2 < lon1:
+                lon2 = lon2+360.
+            distance = lon2-lon1
+            return distance
+        
         west = self.west
         east = self.east
         south = self.south
         north = self.north
         nrows = self.nrows
         ncols = self.ncols
+        xgrid = self.xgrid
+        ygrid = self.ygrid
+        xmesh = self.xmesh
+        ymesh = self.ymesh
+        grid_size = self.grid_size
+        oversampling_list = self.oversampling_list
+        nvar_oversampling = len(oversampling_list)
+        error_model = self.error_model
+        
+        max_ncol = np.round(360/grid_size)
+        
         tmplon = l2g_data['lonc']-west
         tmplon[tmplon < 0] = tmplon[tmplon < 0]+360
         validmask = (tmplon >= 0) & (tmplon <= east-west) &\
@@ -131,18 +165,14 @@ class popy(object):
         nl2 = len(l2g_data['latc'])
         
         #construct a rectangle envelopes the orginal pixel
-        xmargin = 3  #how many times to extend zonally
-        ymargin = 2 #how many times to extend meridonally
+        xmargin = 5  #how many times to extend zonally
+        ymargin = 5 #how many times to extend meridonally
         
-        vcd_A = np.zeros((nrows,ncols))
-        error_A = vcd_A
-        albedo_A = vcd_A
-        cloud_fraction_A = vcd_A
-        cloud_height_A = vcd_A
-        B = vcd_A
-        D = vcd_A
+        mean_sample_weight = np.zeros((nrows,ncols))
+        num_samples = np.zeros((nrows,ncols))
+        sum_aboves = np.zeros((nrows,ncols,nvar_oversampling))
         
-        for il2 in range(nl2-1):
+        for il2 in range(nl2):
             local_l2g_data = {k:v[il2,] for (k,v) in l2g_data.items()}
             if self.sensor_name in {"OMI","OMPS","GOME","GOME2","SCIAMACHY"}:
                 latc = local_l2g_data['latc']
@@ -150,40 +180,86 @@ class popy(object):
                 latr = local_l2g_data['latr']
                 lonr = local_l2g_data['lonr']
                 
-                if lonc < west:
-                    lonc = lonc+360
-                    lonr = lonr+360
+                lonc_index = np.argmin(np.abs(xgrid-lonc))
+                latc_index = np.argmin(np.abs(ygrid-latc))
                 
-                lat_min = latr.min()
-                lon_min = lonr.min()
+                west_extent = np.round(
+                        np.max([F_lon_distance(lonr[0],lonc),F_lon_distance(lonr[1],lonc)])
+                        /grid_size*xmargin)
+                east_extent = np.round(
+                        np.max([F_lon_distance(lonc,lonr[2]),F_lon_distance(lonc,lonr[3])])
+                        /grid_size*xmargin)
                 
-                local_left = lonc-xmargin*(lonc-lon_min)
-                local_right = lonc+xmargin*(lonc-lon_min)
+                lon_index = np.arange(lonc_index-west_extent,lonc_index+east_extent+1,dtype = int)
+                lon_index[lon_index < 0] = lon_index[lon_index < 0]+max_ncol
+                lon_index[lon_index >= max_ncol] = lon_index[lon_index >= max_ncol]-max_ncol
+                lon_index = lon_index[lon_index < ncols]
                 
-                local_bottom = latc-ymargin*(latc-lat_min)
-                local_top = latc+ymargin*(latc-lat_min)
+                patch_west = xgrid[lon_index[0]]
                 
-                vcd = local_l2g_data['vcd']
-                vcde = local_l2g_data['vcde']
+                north_extent = np.ceil((latr.max()-latr.min())/2/grid_size*ymargin)
+                south_extent = north_extent
+                
+                lat_index = np.arange(latc_index-south_extent,latc_index+north_extent+1,dtype = int)
+                lat_index = lat_index[(lat_index > 0) & (lat_index < nrows)]
+                
+                patch_xmesh = F_reference2west(patch_west,xmesh[lat_index,:][:,lon_index])
+                patch_ymesh = ymesh[lat_index,:][:,lon_index]
+                patch_lonr = F_reference2west(patch_west,lonr)
+                patch_lonc = F_reference2west(patch_west,lonc)
+                # this makes num_samples not exactly accurate, may try sum(SG[:])
+                area_weight = Polygon(np.column_stack([patch_lonr[:],latr[:]])).area
+                
+                SG = self.F_2D_SG_transform(patch_xmesh,patch_ymesh,patch_lonr,latr,
+                                            patch_lonc,latc)
+                
+            num_samples[np.ix_(lat_index,lon_index)] =\
+            num_samples[np.ix_(lat_index,lon_index)]+SG
+            
+            if error_model == "square":
+                uncertainty_weight = local_l2g_data['vcde']**2
+            elif error_model == "log":
+                uncertainty_weight = np.log10(local_l2g_data['vcde'])
+            else:
+                uncertainty_weight = local_l2g_data['vcde']
+            mean_sample_weight[np.ix_(lat_index,lon_index)] =\
+            mean_sample_weight[np.ix_(lat_index,lon_index)]+\
+            SG/area_weight/uncertainty_weight
+            for ivar in range(nvar_oversampling):
+                local_var = local_l2g_data[oversampling_list[ivar]]
+                if error_model == 'log':
+                    if oversampling_list[ivar] == 'vcd':
+                        local_var = np.log10(local_var)
+                tmp_var = SG/area_weight/uncertainty_weight*local_var
+                tmp_var = tmp_var[:,:,np.newaxis]
+                sum_aboves[np.ix_(lat_index,lon_index,[ivar])] =\
+                sum_aboves[np.ix_(lat_index,lon_index,[ivar])]+tmp_var
+                
+            return sum_aboves, mean_sample_weight, num_samples
 
 
 
 ### testing
-omi_popy = popy(sensor_name="OMI",grid_size=0.1,west=-10,east=10,south=-10,north=10)
-l2g_data = {'lonc':np.float32([0,1]),'latc':np.float32([0,1]),
-            'lonr':np.float32([[-2,-2,2,2],[-1,-1,3,3]]),
-            'latr':np.float32([[-1,1,1,-1],[0,2,2,0]]),
+omi_popy = popy(sensor_name="OMI",grid_size=1,west=-180,east=180,south=-30,north=30)
+l2g_data = {'lonc':np.float32([-175,175]),'latc':np.float32([0,10]),
+            'lonr':np.float32([[175, 170, -165, -160],[165, 160, -175, -170]]),
+            'latr':np.float32([[-5, 5, 5, -5],[5, 15, 15, 5]]),
             'vcd':np.float32([0.5,1]),'vcde':np.float32([0.25,.5]),
             'albedo':np.float32([0.5,0.2]),'cloud_fraction':np.float32([0.,0.]),
-            'cloud_height':np.float32([1,1]),}
+            'cloud_height':np.float32([1,1])}
 
-sg = omi_popy.F_generalized_SG(omi_popy.xmesh,omi_popy.ymesh,5,5)
-sg1 = omi_popy.F_2D_SG_rotate(omi_popy.xmesh,omi_popy.ymesh,-2,3,5,5,np.pi/4)
-x_r = np.float32([-2,-2,2,2])
-y_r = np.float32([-2,0,1,-1])
-x_c = 0;y_c = 0;
-sg2 = omi_popy.F_2D_SG_transform(omi_popy.xmesh,omi_popy.ymesh,x_r,y_r,x_c,y_c)
+sum_aboves, mean_sample_weight, num_samples = omi_popy.F_regrid(l2g_data)
+
 import matplotlib.pyplot as plt
-plt.contour(omi_popy.xmesh,omi_popy.ymesh,sg2)
-plt.plot(x_c,y_c,'o')
-plt.plot(x_r,y_r,'*')
+plt.contour(omi_popy.xgrid,omi_popy.ygrid,num_samples)
+plt.colorbar
+#sg = omi_popy.F_generalized_SG(omi_popy.xmesh,omi_popy.ymesh,5,5)
+#sg1 = omi_popy.F_2D_SG_rotate(omi_popy.xmesh,omi_popy.ymesh,-2,3,5,5,np.pi/4)
+#x_r = np.float32([-2,-2,2,2])
+#y_r = np.float32([-2,0,1,-1])
+#x_c = 0;y_c = 0;
+#sg2 = omi_popy.F_2D_SG_transform(omi_popy.xmesh,omi_popy.ymesh,x_r,y_r,x_c,y_c)
+#import matplotlib.pyplot as plt
+#plt.contour(omi_popy.xmesh,omi_popy.ymesh,sg2)
+#plt.plot(x_c,y_c,'o')
+#plt.plot(x_r,y_r,'*')
