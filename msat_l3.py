638fa3df0c84b6b67d776d87e51a560d4a5a5dce
Nicholas LoFaso <nlofaso@methanesat.org>
2021-05-06 11:12:36 -0400

    only required packages in environment.yml

--- msat_l3.py
+++ msat_l3.py
@@ -9,7 +9,7 @@ import sys
 import os
 
 
-def run_popy(l2_dir: str, output_dir: str, m: popy, ncores=8: int, block_length=300: int):
+def run_popy(l2_dir: str, output_dir: str, m: popy, ncores: int = 8, block_length: int = 300):
     # read level 2 data in RAM
     m.F_subset_MethaneSAT(l2_dir)
     # optionally save level 2 data subset
@@ -24,46 +24,36 @@ def run_popy(l2_dir: str, output_dir: str, m: popy, ncores=8: int, block_length=
     plt.savefig(os.path.join(output_dir, 'l3.png'))
 
 
-def flag_empty_args(args):
-    if not args.l2_input_dir:
-        raise ValueError("--l2-input-dir must be set")
-
-    if not args.output_dir:
-        raise ValueError("--output-dir must be set")
-
-    if not args.start_time:
-        raise ValueError("--start-time must be set")
-
-    if not args.end_time:
-        raise ValueError("--end-time must be set")
-
-
 def main():
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser(
         description='Level 3 Processing')
 
-    group = parser.add_argument('-i', '--l2-input-dir', help='input directory')
-    group = parser.add_argument('-o', '--output-dir', help='output directory')
-    group = parser.add_argument('--start-time', help='ISO-8601 start time')
-    group = parser.add_argument('--end-time', help='ISO-8601 end time')
     group = parser.add_argument(
-        '-w', '--west', type=float, help='grid west boundary')
+        '-i', '--l2-input-dir', required=True, help='input directory')
     group = parser.add_argument(
-        '-e', '--east', type=float, help='grid east boundary')
+        '-o', '--output-dir', required=True, help='output directory')
     group = parser.add_argument(
-        '-n', '--north', type=float, help='grid north boundary')
+        '--start-time', required=True, help='ISO-8601 start time')
     group = parser.add_argument(
-        '-s', '--south', type=float, help='grid south boundary')
+        '--end-time', required=True, help='ISO-8601 end time')
+    group = parser.add_argument(
+        '-w', '--west', required=True, type=float, help='grid west boundary')
+    group = parser.add_argument(
+        '-e', '--east', required=True, type=float, help='grid east boundary')
+    group = parser.add_argument(
+        '-n', '--north', required=True, type=float, help='grid north boundary')
+    group = parser.add_argument(
+        '-s', '--south', required=True, type=float, help='grid south boundary')
     group = parser.add_argument(
         '--grid-size', type=float, default=0.001, help='grid size in degree')
     group = parser.add_argument(
         '--num-cores', type=int, default=8, help='number of cores to use')
     group = parser.add_argument(
         '--block-length', type=int, default=300, help='block length')
+
     args = parser.parse_args()
-    flag_empty_args(args)
 
     start_time = dateutil.parser.parse(args.start_time)
     end_time = dateutil.parser.parse(args.end_time)
@@ -77,7 +67,7 @@ def main():
              grid_size=args.grid_size)
 
     run_popy(l2_dir=args.l2_input_dir, output_dir=args.output_dir, m=m,
-             num_cores=args.num_cores, block_length=args.block_length)
+             ncores=args.num_cores, block_length=args.block_length)
 
 
 if __name__ == "__main__":
