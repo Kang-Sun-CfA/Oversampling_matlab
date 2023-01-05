0223d5033c7d06a935feb5b095231c07774256f7
Kang Sun <kangsun@buffalo.edu>
2023-01-04 21:59:30 -0500

    tropomi to omi

--- popy.py
+++ popy.py
@@ -3656,6 +3656,187 @@ class popy(object):
                 /(l3_data0['total_sample_weight']
                 +l3_data1['total_sample_weight'])
         return l3_data
+    
+    def F_tropomi2omi(self,omi_pix_1based=None,tcoverage_threshold=None):
+        '''sythesize omi l2g data from tropomi
+        omi_pix_1based:
+            1-based OMI across-track positions to simulate, default 1-60
+        tcoverage_threshold:
+            minimum fraction of valid tropomi pixels in an omi-like envelope
+        written on 2023/01/04
+        '''
+        if self.instrum != 'TROPOMI' and self.product != 'NO2':
+            self.logger.warning('only s5pno2 is tested')
+        l2g = self.l2g_data
+        l2g_omi = dict(lonc=np.array([]),latc=np.array([]),
+                      lonr=np.ndarray(shape=(0,4)),latr=np.ndarray(shape=(0,4)),
+                      column_amount=np.array([]),across_track_position=np.array([]),
+                       UTC_matlab_datenum=np.array([]),surface_altitude=np.array([]),
+                      orbit=np.array([]),column_uncertainty=np.array([]))
+        # row anomaly: omi_pix_1based = np.arange(5,24)
+        omi_pix_1based = omi_pix_1based or np.arange(1,61)
+        tcoverage_threshold = tcoverage_threshold or .8
+        # 60-element list registering 1-based s5pno2 across-track positions (1-450) to the omi ones
+        tpix_oidx = [[1, 2, 3, 4, 5, 6, 7],
+         [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+         [24, 25, 26, 27, 28, 29, 30, 31],
+         [32, 33, 34, 35, 36, 37, 38, 39],
+         [40, 41, 42, 43, 44, 45, 46, 47],
+         [48, 49, 50, 51, 52, 53, 54, 55],
+         [56, 57, 58, 59, 60, 61, 62],
+         [63, 64, 65, 66, 67, 68, 69, 70],
+         [71, 72, 73, 74, 75, 76, 77, 78],
+         [79, 80, 81, 82, 83, 84, 85],
+         [86, 87, 88, 89, 90, 91, 92],
+         [93, 94, 95, 96, 97, 98, 99, 100],
+         [101, 102, 103, 104, 105, 106, 107],
+         [108, 109, 110, 111, 112, 113, 114],
+         [115, 116, 117, 118, 119, 120, 121],
+         [122, 123, 124, 125, 126, 127, 128],
+         [129, 130, 131, 132, 133, 134, 135],
+         [136, 137, 138, 139, 140, 141, 142],
+         [143, 144, 145, 146, 147, 148, 149],
+         [150, 151, 152, 153, 154, 155, 156],
+         [157, 158, 159, 160, 161, 162, 163],
+         [164, 165, 166, 167, 168, 169],
+         [170, 171, 172, 173, 174, 175, 176],
+         [177, 178, 179, 180, 181, 182, 183],
+         [184, 185, 186, 187, 188, 189, 190],
+         [191, 192, 193, 194, 195, 196],
+         [197, 198, 199, 200, 201, 202, 203],
+         [204, 205, 206, 207, 208, 209],
+         [210, 211, 212, 213, 214, 215, 216],
+         [217, 218, 219, 220, 221, 222, 223],
+         [224, 225, 226, 227, 228, 229],
+         [230, 231, 232, 233, 234, 235, 236],
+         [237, 238, 239, 240, 241, 242, 243],
+         [244, 245, 246, 247, 248, 249],
+         [250, 251, 252, 253, 254, 255, 256],
+         [257, 258, 259, 260, 261, 262, 263],
+         [264, 265, 266, 267, 268, 269],
+         [270, 271, 272, 273, 274, 275, 276],
+         [277, 278, 279, 280, 281, 282, 283],
+         [284, 285, 286, 287, 288, 289, 290],
+         [291, 292, 293, 294, 295, 296, 297],
+         [298, 299, 300, 301, 302, 303],
+         [304, 305, 306, 307, 308, 309, 310],
+         [311, 312, 313, 314, 315, 316, 317],
+         [318, 319, 320, 321, 322, 323, 324, 325],
+         [326, 327, 328, 329, 330, 331, 332],
+         [333, 334, 335, 336, 337, 338, 339],
+         [340, 341, 342, 343, 344, 345, 346],
+         [347, 348, 349, 350, 351, 352, 353, 354],
+         [355, 356, 357, 358, 359, 360, 361],
+         [362, 363, 364, 365, 366, 367, 368, 369],
+         [370, 371, 372, 373, 374, 375, 376],
+         [377, 378, 379, 380, 381, 382, 383, 384],
+         [385, 386, 387, 388, 389, 390, 391, 392],
+         [393, 394, 395, 396, 397, 398, 399, 400],
+         [401, 402, 403, 404, 405, 406, 407, 408],
+         [409, 410, 411, 412, 413, 414, 415, 416],
+         [417, 418, 419, 420, 421, 422, 423, 424],
+         [425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436],
+         [437, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450]]
+        max_ntpix = np.array([len(l) for l in tpix_oidx])*2
+        orbits = np.unique(l2g['orbit'])
+        for orbit in orbits:
+            #1.08 vs 0.84 s frametime, changed on 2019/08/06
+            if orbit < 9388:
+                frame_sec = 1.08
+            else:
+                frame_sec = 0.84
+            mask = (l2g['orbit'] == orbit) & (l2g['qa_value']>0.75)
+            dn = l2g['UTC_matlab_datenum'][mask]
+            tlonc = l2g['lonc'][mask]
+            tlatc = l2g['latc'][mask]
+            tlonr = l2g['lonr'][mask,]
+            tlatr = l2g['latr'][mask,]
+            th = l2g['surface_altitude'][mask]
+            tvcd = l2g['column_amount'][mask]
+            tvcde = l2g['column_uncertainty'][mask]
+            udn = np.unique(dn)*86400
+            all_lines = np.round((dn*86400-np.min(udn))/frame_sec).astype(int)
+            all_pixs = l2g['across_track_position'][mask]
+
+            lines = np.arange(np.min(all_lines),np.max(all_lines)-1,2)
+
+            ntpix = np.zeros((len(lines),60))
+            oolonc = np.zeros((len(lines),60))
+            oolatc = np.zeros((len(lines),60))
+
+            oolonr = np.zeros((len(lines),60,4))
+            oolatr = np.zeros((len(lines),60,4))
+
+            oovcd = np.zeros((len(lines),60))
+            oovcde = np.zeros((len(lines),60))
+            oopix = np.zeros((len(lines),60))
+            oodn = np.zeros((len(lines),60))
+            ooh = np.zeros((len(lines),60))
+            ooorbit = np.zeros((len(lines),60))
+            if_all_corners = np.zeros((len(lines),60),dtype=bool)
+
+            for iline, line in enumerate(lines):
+                for opix in omi_pix_1based:
+                    omask = np.isin(all_lines,[line,line+1]) & np.isin(all_pixs,tpix_oidx[int(opix)-1])
+                    ntpix[iline,int(opix)-1] = np.sum(omask)/max_ntpix[int(opix)-1]
+                    if ntpix[iline,int(opix)-1] < tcoverage_threshold:continue
+                    tolonr = tlonr[omask,]
+                    tolatr = tlatr[omask,]
+                    tolonc = tlonc[omask]
+                    tolatc = tlatc[omask]
+                    tovcd = tvcd[omask]
+                    tovcde = tvcde[omask]
+                    tolines = all_lines[omask]
+                    topixs = all_pixs[omask]
+
+                    ll_idx = (topixs == np.min(topixs)) & (tolines == np.min(tolines))
+                    ul_idx = (topixs == np.min(topixs)) & (tolines == np.max(tolines))
+                    ur_idx = (topixs == np.max(topixs)) & (tolines == np.max(tolines))
+                    lr_idx = (topixs == np.max(topixs)) & (tolines == np.min(tolines))
+
+                    # make sure all 4 corners exist
+                    if_all_corner = np.sum(ll_idx)> 0 and np.sum(ul_idx)> 0 and np.sum(ur_idx)> 0 and np.sum(lr_idx)> 0
+
+                    if_all_corners[iline,int(opix)-1] = if_all_corner
+                    if not if_all_corner:continue
+                    olonr = np.array([tolonr[ll_idx,0],tolonr[ul_idx,1],tolonr[ur_idx,2],tolonr[lr_idx,3]]).squeeze()
+                    olatr = np.array([tolatr[ll_idx,0],tolatr[ul_idx,1],tolatr[ur_idx,2],tolatr[lr_idx,3]]).squeeze()
+                    olonc = np.nanmean(tolonc)
+                    olatc = np.nanmean(tolatc)
+                    ovcd = np.nanmean(tovcd)
+                    ovcde = np.nanmean(tovcde)
+                    odn = np.nanmean(dn[omask])
+
+                    oolonc[iline,int(opix)-1] = olonc
+                    oolatc[iline,int(opix)-1] = olatc
+
+                    oolonr[iline,int(opix)-1,] = olonr
+                    oolatr[iline,int(opix)-1,] = olatr
+
+                    oovcd[iline,int(opix)-1] = ovcd
+                    oovcde[iline,int(opix)-1] = ovcde
+                    oodn[iline,int(opix)-1] = odn
+                    ooh[iline,int(opix)-1] = np.nanmean(th[omask])
+                    oopix[iline,int(opix)-1] = opix
+                    ooorbit[iline,int(opix)-1] = orbit
+
+            omi_orbit_mask = (ntpix >= tcoverage_threshold) & if_all_corners
+            l2g_omi['latc'] = np.concatenate([l2g_omi['latc'],oolatc[omi_orbit_mask]])
+            l2g_omi['lonc'] = np.concatenate([l2g_omi['lonc'],oolonc[omi_orbit_mask]])
+
+            l2g_omi['latr'] = np.concatenate([l2g_omi['latr'],np.array([oolatr[...,i][omi_orbit_mask] for i in range(4)]).T])
+            l2g_omi['lonr'] = np.concatenate([l2g_omi['lonr'],np.array([oolonr[...,i][omi_orbit_mask] for i in range(4)]).T])
+
+            l2g_omi['column_amount'] = np.concatenate([l2g_omi['column_amount'],oovcd[omi_orbit_mask]])
+            l2g_omi['column_uncertainty'] = np.concatenate([l2g_omi['column_uncertainty'],oovcde[omi_orbit_mask]])
+            l2g_omi['surface_altitude'] = np.concatenate([l2g_omi['surface_altitude'],ooh[omi_orbit_mask]])
+
+            l2g_omi['UTC_matlab_datenum'] = np.concatenate([l2g_omi['UTC_matlab_datenum'],oodn[omi_orbit_mask]])
+            l2g_omi['across_track_position'] = np.concatenate([l2g_omi['across_track_position'],oopix[omi_orbit_mask]])
+
+            l2g_omi['orbit'] = np.concatenate([l2g_omi['orbit'],ooorbit[omi_orbit_mask]])
+            self.l2g_data = l2g_omi
+    
     def F_read_S5P_nc(self,fn,data_fields,data_fields_l2g=None):
         """ 
         function to read tropomi's level 2 netcdf file to a dictionary
